69ac61e86631622f21690aad582442e74bac4436
yongbaolinux <yongbaolinux@gmail.com>
2023-10-30 10:56:49 +0800

    fix

--- server/conf.php
+++ server/conf.php
@@ -3,11 +3,11 @@
                 	exit();
                 }
                 return array(
-                    'token'=>'',
+                    'token'=>'ee0f0ae6e9c93c418836a2377922d04a',
                     'timeout'=>1698562497,
-                    'appkey'=>'',
-                    'appsecret'=>'',
+                    'appkey'=>'dingj4bjt7nnp8dwfi6w',
+                    'appsecret'=>'Jj2XFErJkLs3uVfQ7Hdods53XP7FcW9SaKUQUWTVjCE8MoHlAz2YChaLsUApaiLX',
                     'register_token'=>'123456',
-                    'register_aes_key'=>'',
-                    'corpid'=>''
+                    'register_aes_key'=>'xxxxxxxxlvdhntotr3x9qhlbytb18zyz5zxxxxxxxxx',
+                    'corpid'=>'ding80ef5106dc236da9ffe93478753d9884'
                 );
