a6ab1db57ef9f8b7cfa25d244ae900cfbbf34551
yongbaolinux <yongbaolinux@gmail.com>
2023-10-30 10:54:29 +0800

    fix

---
+++ server/conf.php
@@ -0,0 +1,13 @@
+<?php
+                if(!defined('HUB')){
+                	exit();
+                }
+                return array(
+                    'token'=>'',
+                    'timeout'=>1698562497,
+                    'appkey'=>'',
+                    'appsecret'=>'',
+                    'register_token'=>'123456',
+                    'register_aes_key'=>'',
+                    'corpid'=>''
+                );
